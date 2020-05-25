5f192ae147d43613c8d112d2be43ebd15101a940
ionutradu90 <R4du_10nutz>
2020-05-25 14:06:23 +0300

    define Routes and import RouteModule

--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -9,7 +9,7 @@ import { map } from 'rxjs/operators';
 })
 export class ProductService {
 
-  private baseurl = 'http://localhost:8080/api/products';
+  private baseurl = 'http://localhost:8080/api/products?size=100';
 
   constructor(private HttpClient: HttpClient) { }
 
